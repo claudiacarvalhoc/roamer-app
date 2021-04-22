4b6a64b3f7d240ffa45e34ca59a6422b2c4b5e97
Claudia Carvalho C <claudia.crisostomo@farfetch.com>
2021-04-22 09:36:13 +0100

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,13 @@ module.exports = {
     es6: true,
     browser: true,
     jquery: true,
+    'jest/globals': true,
   },
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    'jest',
   ],
   extends: ['airbnb', 'airbnb/hooks'],
   rules: {
@@ -15,5 +17,10 @@ module.exports = {
     'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
