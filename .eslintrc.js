dbca7d100f72338722cab6003e6b1cc359f611e1
Claudia Carvalho C <claudia.crisostomo@farfetch.com>
2021-04-22 01:09:46 +0100

    added eslint-config-airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,19 @@
 module.exports = {
+    env: {
+        "es6": true,
+        "browser": true,
+        "jquery": true
+    },
+    root: true,
     parser: '@typescript-eslint/parser',
     plugins: [
         '@typescript-eslint'
     ],
+    extends: ['airbnb', 'airbnb/hooks'],
+    rules: {
+        // note you must disable the base rule as it can report incorrect errors
+        "no-use-before-define": "off",
+        "@typescript-eslint/no-use-before-define": ["error"],
+        'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+    }
 }
