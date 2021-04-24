0fe1b6a8070f6c382725d2711f8685b7c64eacd8
Claudia Carvalho C <claudia.crisostomo@farfetch.com>
2021-04-24 18:46:37 +0100

    adapt button tests to current pattern

--- src/components/button/index.test.tsx
+++ src/components/button/index.test.tsx
@@ -1,33 +1,20 @@
 import React from 'react';
-import Button, { ButtonProperties } from './index';
+import Button, { ButtonProps } from './index';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 const mockOnClick = jest.fn();
-const baseProps: ButtonProperties = {
-    text: 'Button name',
-    type: 'primary',
-    onClick: mockOnClick,
-};
+const getShallowWrapper = (props: ButtonProps) => shallow(<Button {...props} />);
 
-const getShallowWrapper = (moreProps = {}) => {
-    const props = {
-        ...baseProps,
-        ...moreProps,
-    };
-
-    const {
-        text,
-        type,
-        onClick
-    } = props;
-
-    return shallow(<Button text={text} type={type} onClick={onClick} />);
-};
 
 describe('<Button />', () => {
     it('render correctly with primary type', () => {
-        const component = getShallowWrapper();
+        const props: ButtonProps = {
+            text: 'Button name',
+            type: 'primary',
+            onClick: mockOnClick,
+        };
+        const component = getShallowWrapper(props);
         expect(toJson(component)).toMatchSnapshot();
     });
     it('render correctly with secondary type', () => {
@@ -38,7 +25,12 @@ describe('<Button />', () => {
         expect(toJson(component)).toMatchSnapshot();
     });
     it('should call onClick method when button is clicked', () => {
-        const component = getShallowWrapper();
+        const props: ButtonProps = {
+            text: 'Button name',
+            type: 'primary',
+            onClick: mockOnClick,
+        };
+        const component = getShallowWrapper(props);
         component.simulate('click');
         expect(mockOnClick).toHaveBeenCalled();
     });
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,22 +2,18 @@ import React, { FC } from 'react';
 import cn from 'classnames';
 import styles from './button.module.css';
 
-export interface ButtonProperties {
+export interface ButtonProps {
     text: string;
     type: 'primary' | 'secondary';
     onClick: () => void;
 }
 
-const Button: FC<ButtonProperties> = props => {
-    const {
-        text,
-        type,
-        onClick
-    } = props;
-
-    return (
-        <button className={cn(styles.button, styles[type])} onClick={onClick}>{text}</button>
-    );
-};
+const Button: FC<ButtonProps> = ({ text, type, onClick }) => (
+    <button
+        className={cn(styles.button, styles[type])}
+        onClick={onClick}>
+        {text}
+    </button>
+);
 
 export default Button;
