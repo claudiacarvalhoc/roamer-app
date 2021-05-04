0f3077ba842e6bfd8627404f0f1bec9d4624a323
Claudia Carvalho C <claudia.crisostomo@farfetch.com>
2021-05-04 14:22:36 +0100

    build to github pages

--- src/redux/app/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { MenuTextsState, ProjectState, ProjectTextsState } from "../appState";
-import { RootState } from "../reducers";
-
-export const isExpanded = (state: RootState): boolean => state.app.expand;
-export const projects = (state: RootState): ProjectState[] => state.app.projects;
-export const menuTexts = (state: RootState): MenuTextsState => state.app.MenuTextsState;
-export const projectTexts = (state: RootState): ProjectTextsState => state.app.projectTexts;
